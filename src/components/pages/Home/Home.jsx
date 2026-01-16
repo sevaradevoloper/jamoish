a59368110fcef8c0ed735832ee06eb3abcf863d9
H - Komila <komilaxibziddinova06@gmail.com>
2026-01-16 22:38:37 +0500

    Article va Silder qo'shildi

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Hero from './Hero/Hero'
+import Slider from '../../Slider/Slider'
+import Article from '../../Article/Article'
 const Home = () => {
   return (
     <>
     <Hero/>
+    <Slider/>
+    <Article/>
     </>
   )
 }
