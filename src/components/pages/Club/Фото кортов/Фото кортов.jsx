d79e2166ff9569302f172dbd16446a26769948af
QorgonovAzizbek <azizbekqorgonov@gmail.com>
2026-01-16 21:21:38 +0500

    adding page

---
+++ src/components/pages/Club/Фото кортов/Фото кортов.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import React from 'react'
+import tailwindcss from '@tailwindcss/vite'
+
+const first_section = () => {
+    return (
+        <main>
+            <section>
+                <h1>Фото кортов</h1>
+
+            </section>
+        </main>
+    )
+}
+
+export default first_section
