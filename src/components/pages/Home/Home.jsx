215a6b9b2a794ea0ebac833b4ac6a67b8e4b6e82
sevara128@icloud.com <sevara128@icloud.com>
2026-01-16 23:24:19 +0300

    ss

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,28 +1,20 @@
 import React from 'react'
 import Hero from './Hero/Hero'
-<<<<<<< HEAD
 import Slider from '../../Slider/Slider'
 import Article from '../../Article/Article'
-
+import Section from './Section/Section'
+import Main from './Main/Main'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Section/>
+      <Main/>
       <Slider />
       <Article />
     </div>
-=======
-import Section from './Section/Section'
-import Main from './Main/Main'
-const Home = () => {
-  return (
-    <>
-    <Hero/>
-    <Section/>
-    <Main/>
-    </>
->>>>>>> 4c9592b9abc397cd23641dc1436b2aa9fa66466e
+
   )
 }
 
