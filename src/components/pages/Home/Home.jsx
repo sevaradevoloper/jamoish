f3a9781bbda0a7b01a47a0ef4361d7a23d07bc62
H - Komila <komilaxibziddinova06@gmail.com>
2026-01-16 23:08:03 +0500

    article

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Hero from './Hero/Hero'
-<<<<<<< HEAD
 import Slider from '../../Slider/Slider'
 import Article from '../../Article/Article'
-
 
 const Home = () => {
   return (
@@ -12,17 +10,6 @@ const Home = () => {
       <Slider />
       <Article />
     </div>
-=======
-import Section from './Section/Section'
-import Main from './Main/Main'
-const Home = () => {
-  return (
-    <>
-    <Hero/>
-    <Section/>
-    <Main/>
-    </>
->>>>>>> 4c9592b9abc397cd23641dc1436b2aa9fa66466e
   )
 }
 
