4784b722bb9a5539c3bff52907b8b9e120d28101
farzona <aceracerov67@gmail.com>
2026-01-16 20:21:34 +0500

    section qoshdim

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import Section from '../Section/Section'
 import { Outlet } from 'react-router-dom'
 const Layout = () => {
   return (
@@ -9,6 +10,7 @@ const Layout = () => {
        <main className='main'>
         <Outlet/>
        </main>
+       <Section/>
        <Footer/>
     </>
   )
